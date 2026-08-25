99b320c4a4735ac8ddecf647ec60566e8e2a3d71
Mohammad RamezaniG <mohammad.ramezaniicpc@gmail.com>
2026-08-25 14:03:55 +0330

    Add exercise and mini-proje

---
+++ java-script/day-02/exercise.js
@@ -0,0 +1,140 @@
+// If statement
+
+let age = 17;
+
+if (age >= 18) {
+
+    console.log("you can enter");
+    
+} else {
+
+    console.log("you cannot enter");
+    
+}
+
+let hoursPerDay = 5;
+
+if (hoursPerDay >= 6) {
+
+    console.log("good study schudule.");
+    
+} else{
+
+    console.log("you need to study more");
+    
+}
+
+
+let score = 87;
+
+if (score >= 90) {
+    
+    console.log("exellent");
+    
+} else if (score >= 80) {
+
+    console.log("very good");
+    
+} else if (score >= 70) {
+
+    console.log("good");
+    
+} else {
+
+    console.log("you need improvement");
+    
+}
+
+let age = 19;
+let hoursPerDay =8
+
+if (age >= 18 && hoursPerDay >= 6) {
+
+    console.log("Ready to start");
+    
+}
+
+let hasETH = false;
+let hasUSDC = true;
+
+if (hasETH || hasUSDC) {
+    
+    console.log("Ready for transaction");
+    
+}
+
+let isBusy = false;
+
+
+if (!isBusy) {
+  
+    console.log("availabe");
+    
+}
+
+
+let age = 19;
+let hasWallet = true;
+let hasETH = false;
+let hasUSDC = true;
+
+if (age >= 18 && hasWallet && (hasETH || hasUSDC)) {
+    
+    console.log("transaction allowed");
+    
+}
+
+let age = 19;
+
+if (age !== 20) {
+        
+    console.log(" age is not 20");
+        
+} else{
+
+    console.log("age is 20");
+        
+}
+
+
+let temperature = 25;
+
+if (temperature >= 30) {
+    
+    console.log("Hot");
+    
+} else if (temperature >= 20 ) {
+    
+    console.log("Nice");
+    
+} else {
+
+    console.log("Cold");
+    
+}
+
+let age = 19
+let hoursPerDay = 8
+
+if (age >= 18 && hoursPerDay >= 6) {
+    
+    console.log("Accepted");
+    
+} else {
+
+    console.log("Not accepted");
+    
+}
+
+let hasETH = false;
+let hasUSDC = true;
+
+if (hasETH || hasUSDC) {
+    
+    console.log("Transaction possible");
+    
+} else {
+
+    console.log("Need ETH or USDC");
+    
+}
---
+++ java-script/day-02/mini-proje.js
@@ -0,0 +1,22 @@
+let JavaScriptHours = 100;
+let solidityHours = 40;
+let hasGitHub = true;
+let hasGit = true;
+
+if (JavaScriptHours < 50) {
+    
+    console.log("learn more JavaScript");
+    
+} else if (!hasGit) {
+    
+    console.log("Learn Git");
+    
+} else if (solidityHours < 50) {
+    
+    console.log("Start learning Solidity");
+    
+} else {
+
+    console.log("Ready for next stage");
+    
+}
