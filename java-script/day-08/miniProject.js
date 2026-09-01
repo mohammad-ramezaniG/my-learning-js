93fd59af76b70db7df5c156c80a0c3bddff9ee0d
Mohammad RamezaniG <mohammad.ramezaniicpc@gmail.com>
2026-09-01 16:40:27 +0330

    add day-09 test

--- java-script/day-08/miniProject.js
+++ java-script/day-08/miniProject.js
@@ -18,3 +18,28 @@ function upgradeDeveloper({name , skills}, ...newSkills) {
 let newDeveloper = upgradeDeveloper(developer, "Web3" , "Git" );
 
 console.log(newDeveloper);
+
+
+let developer1 = {
+    name: "Mohammad",
+    age: 19,
+    skills: ["JavaScript" , "Solidity"]
+};
+
+function upgradeDeveloper({name , skills} , ...newSkills) {
+    
+    return {
+        name: name,
+        upgradedSkills: [
+            ...skills,
+            ...newSkills
+        ]
+    };
+
+}
+
+upgradeDeveloper(developer1 , "Web3" , "Git")
+
+let call = upgradeDeveloper(developer1 , "Web3" , "Git");
+
+console.log(call);
